82c6216b395258a084b66bf5085add677511cefb
EyobTM <52195168+EyobMengiste@users.noreply.github.com>
2022-01-14 18:13:56 +0400

    Add files via upload

---
+++ final_pcd_classifier.py
@@ -0,0 +1,90 @@
+import numpy as np
+from pyntcloud import PyntCloud
+import os
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import tensorflow as tf
+from keras import optimizers
+from keras.layers import Input
+from keras.models import Model
+from keras.layers import Dense, Reshape
+from keras.layers import Convolution1D, MaxPooling1D, BatchNormalization
+from keras.layers import Lambda, concatenate
+import h5py
+import glob 
+import trimesh
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import open3d 
+
+
+
+
+def conv_bn(x, filters):
+    x = layers.Conv1D(filters, kernel_size=1, padding="valid")(x) # 1D convolution. 2D  conv means sampling the dataset with colors 
+    x = layers.BatchNormalization(momentum=0.0)(x)
+    return layers.Activation("relu")(x)
+
+
+def dense_bn(x, filters):
+    x = layers.Dense(filters)(x)
+    x = layers.BatchNormalization(momentum=0.0)(x)
+    return layers.Activation("relu")(x)
+
+
+class OrthogonalRegularizer(keras.regularizers.Regularizer):
+    def __init__(self, num_features, l2reg=0.001):
+        self.num_features = num_features
+        self.l2reg = l2reg
+        self.eye = tf.eye(num_features)
+
+    def __call__(self, x):
+        x = tf.reshape(x, (-1, self.num_features, self.num_features))
+        xxt = tf.tensordot(x, x, axes=(2, 2))
+        xxt = tf.reshape(xxt, (-1, self.num_features, self.num_features))
+        return tf.reduce_sum(self.l2reg * tf.square(xxt - self.eye))
+    
+    
+def tnet(inputs, num_features):
+    
+    
+    bias = keras.initializers.Constant(np.eye(num_features).flatten())
+    reg = OrthogonalRegularizer(num_features)
+
+    x = conv_bn(inputs, 32)
+    x = conv_bn(x, 64)
+    x = conv_bn(x, 512)
+    x = layers.GlobalMaxPooling1D()(x) # change to 2D 
+    x = dense_bn(x, 256)
+    x = dense_bn(x, 128)
+    x = layers.Dense(
+        num_features * num_features,
+        kernel_initializer="zeros",
+        bias_initializer=bias,
+        activity_regularizer=reg,
+    )(x)
+    feat_T = layers.Reshape((num_features, num_features))(x)
+    return layers.Dot(axes=(2, 1))([inputs, feat_T])
+
+
+inputs = keras.Input(shape=(3000, 3))
+
+x = tnet(inputs, 3)
+x = conv_bn(x, 32)
+x = conv_bn(x, 32)
+x = tnet(x, 32)
+x = conv_bn(x, 32)
+x = conv_bn(x, 64)
+x = conv_bn(x, 512)
+x = layers.GlobalMaxPooling1D()(x)
+x = dense_bn(x, 256)
+x = layers.Dropout(0.3)(x)
+x = dense_bn(x, 128)
+x = layers.Dropout(0.3)(x)
+
+outputs = layers.Dense(8, activation="softmax")(x)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
